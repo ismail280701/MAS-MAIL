caa4870238265b2e8e848fefdca4f3b7b2834a24
ismail280701 <ismail2807011@gmail.com>
2026-09-03 09:06:30 +0700

    Add event listener for DOMContentLoaded

--- js/digital.js
+++ js/digital.js
@@ -1,1 +1,19 @@
+"use strict";
 
+/*
+ * =========================================
+ * MAS MAIL DIGITAL
+ * DIGITAL.JS
+ * =========================================
+ */
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+        console.log(
+            "Mas Mail Digital — halaman aktif."
+        );
+
+    }
+);
